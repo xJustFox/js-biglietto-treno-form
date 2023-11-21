6dc078710735bc5dcb5ce14f3af3fa685bdb572d
xJustFox <dodydaversa@gmail.com>
2023-11-21 14:40:35 +0100

    [Update] else if

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 const button = document.querySelector(".btn-send");
 
-button.addEventListener("click", function() {
+button.addEventListener("click", function () {
     let user = document.getElementById("user").value;
     let distance_km = document.getElementById("distance_km").value;
     let age = document.getElementById("age").value;
-    
+
     let price_new = distance_km * 0.21;
-    
+
     console.log(user);
     console.log(distance_km);
     console.log(age);
     
+    // Sè l'utente avrà meno di 18 anni verra applicato il 20% di sconto
+    if (age == "underage") {
+        discount = (price_new * 20) / 100;
+        price_new -= discount
+    }
+    // Sè l'utente avrà più di 18 anni ma meno di 65 verra applicato il 40% di sconto
+    else if (age == "over65") {
+        discount = (price_new * 40) / 100;
+        price_new -= discount
+    }
 })
