c1cedbb9347971d35d46c1af94624a4c9e17c18f
xJustFox <dodydaversa@gmail.com>
2023-11-21 14:39:16 +0100

    [Upgrade]var user distance_km age price_new

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 const button = document.querySelector(".btn-send");
 
 button.addEventListener("click", function() {
-
+    let user = document.getElementById("user").value;
+    let distance_km = document.getElementById("distance_km").value;
+    let age = document.getElementById("age").value;
+    
+    let price_new = distance_km * 0.21;
+    
+    console.log(user);
+    console.log(distance_km);
+    console.log(age);
+    
 })
